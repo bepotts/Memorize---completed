0ddbba54262f4fa21de44665ff1830f03c00b8a3
Brandon Potts <ObeliskIsHuge@gmail.com>
2025-04-16 20:47:28 -0400

    intial commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Brandon Potts on 3/5/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+Assignment I: Memorize ( CS 193P )
+====
+
+Completed Code for Stanford CS 193P 
+
+Currently in progress
+
